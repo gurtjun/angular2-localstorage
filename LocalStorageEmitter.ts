1e69421b22d711e4f03846d225b4b1f9e27cfb9e
Gert-jan <gurtjun@users.noreply.github.com>
2016-09-07 18:38:55 +0200

    Update to RC6

--- LocalStorageEmitter.ts
+++ LocalStorageEmitter.ts
@@ -43,12 +43,10 @@ export class LocalStorageService implements OnDestroy {
     }
 }
 
-import {Type} from "@angular/core/src/facade/lang";
-import {provide} from "@angular/core/src/di";
-import {ComponentRef} from "@angular/core";
+import {ComponentRef, Type} from "@angular/core";
 
 export function LocalStorageSubscriber(appPromise: Promise<ComponentRef<any>>) {
     appPromise.then((bla) => {
-        bla.injector.get(<Type>LocalStorageService);
+        bla.injector.get(<Type<any>>LocalStorageService);
     });
 }
